f68f992d1425fa34324b470552996681c436ff59
ITHS <martinpaulsen617@gmail.com>
2025-04-23 16:24:28 +0200

    Databasfilen

---
+++ src/main/java/org/example/Database.java
@@ -0,0 +1,19 @@
+package org.example;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface Database {
+    int getPlayerIdByName(String name) throws SQLException;
+    void saveResult(int playerId, int guesses) throws SQLException;
+    List<DatabaseManager.PlayerAverage> getTopTenPlayers() throws SQLException;
+
+    class PlayerAverage{
+        public final String name;
+        public final double average;
+
+        public PlayerAverage(String name, double average) {
+            this.name = name;
+            this.average = average;
+        }
+    }
+}
