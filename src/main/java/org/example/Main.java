36b47553bd07d6772280a803bd9fd5a5ddea4048
ITHS <martinpaulsen617@gmail.com>
2025-04-23 16:21:18 +0200

    Filer som inte skickades med

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        try {
+            UserInterface ui = new SimpleWindowUI("Moo");
+            Database db = new DatabaseManager(
+                    "jdbc:mysql://localhost/Moo", "root", "MartinPaulsen03");
+            Game game = new StandardMooGame();
+
+            GameController controller = new GameController(ui, db, game);
+            controller.run();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/example/UserInterface.java
+++ src/main/java/org/example/UserInterface.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public interface UserInterface {
+    void showMessage(String message);
+    String prompt(String message);
+    boolean askYesNo(String message);
+    void clear();
+    void exit();
 }
